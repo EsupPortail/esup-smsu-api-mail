47c9973eba0130fa44b7470b07cfd69c922ff522
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2014-05-20 17:09:56 +0200

    WIP since new smsuapi sends all SMS at once, there is no need to handle messageId anymore in smsuapimail

--- src/main/java/org/esupportail/smsuapimail/services/smsSender/ISmsSender.java
+++ src/main/java/org/esupportail/smsuapimail/services/smsSender/ISmsSender.java
@@ -1,14 +1,5 @@
 package org.esupportail.smsuapimail.services.smsSender;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.util.List;
 
 import org.esupportail.commons.services.logging.Logger;
@@ -24,17 +15,6 @@ public class ISmsSender {
 	private final Logger logger = new LoggerImpl(getClass());
 
 	@Autowired private HttpRequestSmsuapiWS ws;
-
-	/**
-	 * a file to save and initialize the message ID
-	 */
-	private String fileName;
-
-
-	/**
-	 * Message id sent to the back office.
-	 */
-	private Integer messageId;
 	
 	/**
 	 * Sender id sent to the back office.
@@ -61,8 +41,6 @@ public class ISmsSender {
 		final int nbSmsToSend = phoneNumbers.size();
 		
 		checkQuota(nbSmsToSend, accountLabel);
-		messageId ++;
-		saveMessageId();
 		sendMessage(phoneNumbers, message, accountLabel);
 	}
 	
@@ -102,31 +80,14 @@ public class ISmsSender {
 		
 		if (logger.isDebugEnabled()) {
 			logger.debug("Sending request sendSMS to back office with parameters : \n" + 
-				     " - Message id : " + messageId + "\n" + 
 				     " - Sender id : " + senderId + "\n" + 
 				     " - Phone number : " + phoneNumbers + "\n" + 
 				     " - Account label : " + accountLabel + "\n" + 
 				     " - content : " + content);
 		}
 		
-		ws.sendSms(messageId, phoneNumbers, content, accountLabel, senderId);
-	}
-	
-	/**
-	 * save the current messageID in a file.
-	 */
-	private void saveMessageId() {
-		PrintWriter pw;
-	    
-		try {
-			pw =  new PrintWriter(new BufferedWriter
-			   (new FileWriter(fileName)));
-			pw.println(messageId);
-			pw.close();
-		} catch (IOException e) {
-			logger.warn("Enable to save messageId to file " + fileName, e);
-		}
-	}
+		ws.sendSms(null, phoneNumbers, content, accountLabel, senderId);
+	}	
 	
 	/**
 	 * Standard setter used by Spring.
@@ -134,45 +95,5 @@ public class ISmsSender {
 	 */
 	public void setSenderId(final Integer senderId) {
 		this.senderId = senderId;
-	}
-
-	/**
-	 * Standard setter used by Spring.
-	 * @param fileName
-	 */
-	public void setFileName(final String fileName) {
-		this.fileName = fileName;
-		try {
-			File ips=new File(fileName);
-			InputStreamReader ipsr=new FileReader(ips);
-			BufferedReader br=new BufferedReader(ipsr);
-			String ligne;
-			ligne=br.readLine();
-			if (ligne == null){
-				messageId = 1;
-				saveMessageId();
-			} else {
-				try {
-					messageId = Integer.valueOf(ligne);
-				} catch (NumberFormatException e) {
-					messageId = 1;
-					saveMessageId();
-				}
-			}
-			br.close();
-			} catch (FileNotFoundException e) {
-				messageId = 1;
-				saveMessageId();
-			} catch (IOException e) {
-				messageId = 1;
-				saveMessageId();
-			}
-			
-			
-			
-			if (logger.isDebugEnabled()) {
-				logger.debug("MessageId initialised with value : [" + messageId + "]");
-			}
-
 	}
 }
